ea56c9113c994dd478fd399a068830bda271d4ec
MalekEssayeh <Malek.essayeh@esprit.tn>
2024-02-18 11:26:58 +0100

    CRUD Exercice +3 new CSS

--- src/main/java/PilatesPulse/Controller/AjoutExercice.java
+++ src/main/java/PilatesPulse/Controller/AjoutExercice.java
@@ -6,10 +6,13 @@ import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import PilatesPulse.Services.ExerciceService;
 import PilatesPulse.Models.Exercice;
+import javafx.scene.text.Font;
+
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,6 +20,10 @@ import java.util.ResourceBundle;
 public class AjoutExercice implements Initializable {
     @FXML
     private MFXComboBox<String> Muscle;
+    @FXML
+    private Label label1;
+    @FXML
+    private Label label11;
     @FXML
     private MFXSlider Evaluation;
     @FXML
@@ -33,6 +40,15 @@ public class AjoutExercice implements Initializable {
         String[] t2 = {"Facile", "Moyenne", "Difficile"};
         Muscle.getItems().addAll(t1);
         Difficulte.getItems().addAll(t2);
+            // Load your desired font
+        Font customFont = Font.loadFont(getClass().getResourceAsStream("/gothicb.ttf"), 72);
+        Font customFont2 = Font.loadFont(getClass().getResourceAsStream("/gothicb.ttf"), 18);
+
+            // Set the font to the labels
+            label1.setFont(customFont);
+            label11.setFont(customFont2);
+
+
     }
 
 
