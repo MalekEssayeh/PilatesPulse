a1a5b9758fd4807ef129e1b9913cce6fd3ea06e4
Your Name <you@example.com>
2024-02-27 00:44:03 +0100

    Metier: Shopping cart

---
+++ src/main/java/tn/PilatePulse/controllers/ShoppingCart.java
@@ -0,0 +1,62 @@
+package tn.PilatePulse.controllers;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.legacy.MFXLegacyListView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class ShoppingCart {
+
+    @FXML
+    private Button checkoutButton;
+
+    @FXML
+    private MFXButton eventsButton;
+
+    @FXML
+    private MFXButton exercicesButton;
+
+    @FXML
+    private MFXButton homeButton;
+
+    @FXML
+    private MFXLegacyListView<?> listOfProducts;
+
+    @FXML
+    private ImageView logoImg;
+
+    @FXML
+    private MFXButton programsButton;
+
+    @FXML
+    private MFXButton searchButton;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private Button shopButton;
+
+    @FXML
+    private AnchorPane topBar;
+
+    @FXML
+    void checkOutFunction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnToShop(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search(ActionEvent event) {
+
+    }
+
+}
